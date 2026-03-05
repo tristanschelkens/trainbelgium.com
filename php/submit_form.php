b831849d4d036d95a6f6d99ceda80f62059b81e0
Tristan_YaTi <tristanschelkens@gmail.com>
2026-03-05 09:18:58 +0100

    fz

--- php/submit_form.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
- 
-    // Voor demonstratiedoeleinden sturen we de gebruiker door naar een bedankpagina
-    header("Location: ../html/Success.html");
-    exit();
-} 
